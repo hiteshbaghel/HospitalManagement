dc18ff299832604206cf0dbc85a59833f4c03e41
Hitesh Baghel <127033375+hiteshbaghel@users.noreply.github.com>
2025-10-21 22:08:26 +0530

    Commit

--- src/main/java/com/hitesh/HospitalManagement/repository/PatientRepository.java
+++ src/main/java/com/hitesh/HospitalManagement/repository/PatientRepository.java
@@ -2,7 +2,9 @@ package com.hitesh.HospitalManagement.repository;
 
 import com.hitesh.HospitalManagement.entity.Patient;
 import com.hitesh.HospitalManagement.type.BloodGroupType;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -20,4 +22,15 @@ public interface PatientRepository extends JpaRepository<Patient,Long> {
     List<Patient> findByNameContainingOrderByIdDesc(String name);
     @Query("SELECT p from Patient p where p.bloodGroup = ?1")
     List<Patient> findByBloodGroup(@Param("bloodGroup")BloodGroupType bloodGroup);
+    @Query("select p from Patient p where p.birthDate > :birthDate")
+    List<Patient> findByBornAfterBirthDate(@Param("birthDate")LocalDate birthDate);
+    @Query("SELECT p.bloodGroup,Count(p) from Patient p group by p.bloodGroup ")
+    List<Object[]> countEachBloodGroup ();
+    @Query(value = "select * from patient", nativeQuery = true)
+    List<Patient> findAllPatient();
+    @Transactional
+    @Modifying
+    @Query("UPDATE Patient p SET p.name = :name where p.id = :id")
+    int patientUpdateNameWithId(@Param("name") String name,@Param("id") Long id);
+
 }
--- src/test/java/com/hitesh/HospitalManagement/PatientTest.java
+++ src/test/java/com/hitesh/HospitalManagement/PatientTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootTest
@@ -16,25 +17,44 @@ public class PatientTest {
     private PatientRepository patientRepository;
     @Autowired
     private PatientService patientService;
+
     @Test
-    public void patientRepositoryTest(){
+    public void patientRepositoryTest() {
         List<Patient> patientList = patientRepository.findAll();
         System.out.println(patientList);
     }
 
     @Test
-    public void testTransactionMethod(){
-        //Patient patient = patientService.getPatientById(1L);
-        // Patient patient1 = patientRepository.findById(1L).orElseThrow(()-> new IllegalArgumentException("Patient not found ID : " + patient.getId()));
-        //System.out.println(patient1);
-       // Patient patient = patientRepository.findByName("Neha Iyer");
-      //  List<Patient> patientList = patientRepository.findByBirthDateOrEmail(LocalDate.of(1992,12,1), "kabir.singh@example.com");
-      //  List<Patient> patientList = patientRepository.findByBirthDateBetween(LocalDate.of(1990,5,10),LocalDate.of(1995,8,20));
-       // List<Patient> patientList = patientRepository.findByNameContainingOrderByIdDesc("Di");
-        List<Patient> patientList = patientRepository.findByBloodGroup(BloodGroupType.A_POSITIVE);
-        for (Patient patient : patientList) {
-            System.out.println(patient);
-        }
+    public void testTransactionMethod() {
+//        Patient patient = patientService.getPatientById(1L);
+//
+//        Patient patient1 = patientRepository.findById(1L).orElseThrow(()-> new IllegalArgumentException("Patient not found ID : " + patient.getId()));
+//        System.out.println(patient1);
+//
+//        Patient patient = patientRepository.findByName("Neha Iyer");
+//
+//        List<Patient> patientList = patientRepository.findByBirthDateOrEmail(LocalDate.of(1992,12,1), "kabir.singh@example.com");
+//
+//        List<Patient> patientList = patientRepository.findByBirthDateBetween(LocalDate.of(1990,5,10),LocalDate.of(1995,8,20));
+//
+//        List<Patient> patientList = patientRepository.findByNameContainingOrderByIdDesc("Di");
+//
+//        List<Patient> patientList = patientRepository.findByBloodGroup(BloodGroupType.A_POSITIVE);
+//
+//        List<Patient> patientList = patientRepository.findByBornAfterBirthDate(LocalDate.of(1990,5,10));
+
+//        List<Patient> patientList = patientRepository.findAllPatient();
+//        for (Patient patient : patientList) {
+//            System.out.println(patient);
+//        }
+
+//        List<Object[]> bloudGroupList = patientRepository.countEachBloodGroup();
+//        for(Object [] objects :bloudGroupList){
+//            System.out.println(objects[0]+" "+objects[1]);
+//        }
+
+        int rowsUpdated = patientRepository.patientUpdateNameWithId("Arav Sharma", 1L);
+        System.out.println(rowsUpdated);
 
     }
 }
